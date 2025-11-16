eca17ab5316deafce124860ce48ab6e31c070bf7
rohithnair1996 <rohithmaccs@gmail.com>
2025-11-16 17:17:52 +0530

    Completed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
 
 export default function Header() {
   return (
     <View style={styles.container}>
       <View style={styles.circleView}>
-        <Ionicons name="chevron-back" size={28} color="#ffffff" />
+        <Ionicons name="arrow-back" size={28} color="#ffffff" />
       </View>
-      <View style={styles.childView}>
-        <Text style={styles.text}>v2</Text>
+      <View style={styles.rightContainer}>
+        <View style={styles.iconWithText}>
+          <FontAwesome5 name="user-friends" size={15} color="#ffffff" />
+          <Text style={styles.text}>5</Text>
+        </View>
+        <View style={styles.iconWithText}>
+        <FontAwesome5 name="user-plus" size={15} color="#ffffff" />
+          <Text style={styles.text}>Invite</Text>
+        </View>
       </View>
     </View>
   );
@@ -18,7 +28,7 @@ export default function Header() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    padding: 20,
+    padding: 10,
     backgroundColor: '#1d283a',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -26,17 +36,31 @@ const styles = StyleSheet.create({
     borderBottomColor: '#e0e0e0',
   },
   circleView: {
-    width: 48,
-    height: 48,
-    borderRadius: 24,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
     backgroundColor: '#141c29',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  iconWithText: {
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#141c29',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 10,
+    padding: 10,
+  },
   text: {
-    fontSize: 24,
+    fontSize: 15,
     fontWeight: 'bold',
     color: '#333',
+  },
+  rightContainer: {
+    flexDirection: 'row',
+    gap: 10,
   },
 });
 
